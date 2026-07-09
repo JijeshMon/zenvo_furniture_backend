fa63bac50cb6466818661e3f97da6d34af3dc762
JijeshMon <jijeshjmj@gmail.com>
2026-07-09 20:53:53 +0530

    corsConfig change hardcode to property file

--- zenvofurniture/src/main/java/com/zenvofurniture/config/CorsConfig.java
+++ zenvofurniture/src/main/java/com/zenvofurniture/config/CorsConfig.java
@@ -1,20 +1,20 @@
 package com.zenvofurniture.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+	
+	@Value("${app.cors.allowed-origin}")
+	    private String allowedOrigin;
     
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins(
-            		"http://localhost:3000",
-            	    "http://187.127.189.97" 
-            		
-            )
+            .allowedOrigins(allowedOrigin)
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
             .allowedHeaders("*")
             .allowCredentials(true)
